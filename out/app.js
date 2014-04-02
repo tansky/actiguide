c5ce27511c24123d39e0f4c31b0a92f9830c8cf5
Semyon Vyskubov <sv@rv7.ru>
2014-04-02 17:00:56 +0400

    Grid refactoring

--- out/app.js
+++ out/app.js
@@ -11,20 +11,16 @@ $(document).keydown(function(e) {
 			agGridState = true;
 
 			$('BODY').append(
-					'<div id="in-guidelines">' +
-					'	<div id="guidelines">' +
-					'		<div class="for-guidelines">' +
-					'			<div class="col-edge-1"><div class="box"><p class="grid-p"></p></div></div>' +
-					'			<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
-					'			<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
-					'			<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
-					'			<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
-					'			<div class="col-edge-1"><div class="box"><p class="grid-p"></p></div></div>' +
-					'			<div class="left-padding-grid"></div>' +
-					'			<div class="right-padding-grid"></div>' +
-					'		</div>' +
-					'	</div>' +
-					'</div>'
+				'<div id="in-guidelines"><div id="guidelines"><div class="for-guidelines">' +
+					'<div class="col-edge-1"><div class="box"><p class="grid-p"></p></div></div>' +
+					'<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
+					'<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
+					'<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
+					'<div class="col-1"><div class="box"><p class="grid-p"></p></div></div>' +
+					'<div class="col-edge-1"><div class="box"><p class="grid-p"></p></div></div>' +
+					'<div class="left-padding-grid"></div>' +
+					'<div class="right-padding-grid"></div>' +
+				'</div></div></div>'
 			);
 		}
 	}
