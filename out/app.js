589084589e3493c42f379f64a548d1898a67d31f
Semyon Vyskubov <sv@rv7.ru>
2014-04-02 14:00:35 +0400

    Grid fix

--- out/app.js
+++ out/app.js
@@ -1,18 +1,20 @@
 $(document).keydown(function(e) {
 	if (e && 192 === e.keyCode && e.shiftKey && e.ctrlKey) {
-		if (!$("#in_guidelines").length) {
+		var $inGuidelines = $("#in_guidelines"),
+			$forGuidelines = $(".for_guidelines"),
+			$popupBox = ("#popup_box");
+
+		if (!$inGuidelines.length) {
 			var ctr = '<div id="in_guidelines" style="display:none;"><div id="guidelines"><div class="for_guidelines"><div class="col-edge-1"><div class="box"><p class="grid_p"></p></div></div><div class="col-1"><div class="box"><p class="grid_p"></p></div></div><div class="col-1"><div class="box"><p class="grid_p"></p></div></div><div class="col-1"><div class="box"><p class="grid_p"></p></div></div><div class="col-1"><div class="box"><p class="grid_p"></p></div></div><div class="col-edge-1"><div class="box"><p class="grid_p"></p></div></div></div></div></div>';
 			$("body").prepend(ctr);
 		}
-		var grid = $("#in_guidelines");
-		grid.css("display", "none" === grid.css("display") ? "block" : "none"), $(".left-padding-grid").length || ($(".for_guidelines").append('<div class="left-padding-grid"></div>'),
-		$(".for_guidelines").append('<div class="right-padding-grid"></div>'));
+		$inGuidelines.css("display", "none" === $inGuidelines.css("display") ? "block" : "none"), $(".left-padding-grid").length || ($forGuidelines.append('<div class="left-padding-grid"></div>'), $forGuidelines.append('<div class="right-padding-grid"></div>'));
 	} else {
 		if (!(e && 49 === e.keyCode && e.shiftKey && e.ctrlKey)) return;
 		var popupBox = '<div id="popup_box" style="display:none"></div>', elems = $('i[id], input[type="submit"][id], button[type="submit"]');
-		$("#popup_box").length || $("body").prepend(popupBox), elems.each(function() {
+		$popupBox.length || $("body").prepend(popupBox), elems.each(function() {
 			var markedText = "I" == this.tagName ? $(this).parent() : $(this);
 			markedText.hasClass("marked_text") && markedText.parent().hasClass("marked_text") ? markedText.removeClass("marked_text") : markedText.addClass("marked_text");
-		}), $("#popup_box").css("display", "none" === $("#popup_box").css("display") ? "block" : "none");
+		}), $popupBox.css("display", "none" === $popupBox.css("display") ? "block" : "none");
 	}
 });
