f92c9508e3aa6ff62764f6b1ecf547a9aac5b0e7
Semyon Vyskubov <sv@rv7.ru>
2014-04-22 18:03:31 +0400

    Алертбоксы в тестовой форме

--- out/app.js
+++ out/app.js
@@ -505,47 +505,47 @@ $(document).keydown(function(e) {
 
 
 
-})(jQuery);;actiGuide.mainModule = angular.module('mainModule', []);;actiGuide.mainModule.controller('TestFormCtrl', function ($scope, $timeout) {
+})(jQuery);;actiGuide.mainModule = angular.module('mainModule', []);;actiGuide.mainModule.controller('TestFormCtrl', function ($scope, $timeout, alertBox) {
 
-    $scope.Model = {};
+	$scope.Model = {};
 
-    $scope.Model.PaymentTypeCatalog = [
-        {
-            Name: 'Оплата контрагенту',
-            Id: 1
-        },
-        {
-            Name: 'Возврат контрагенту',
-            Id: 2
-        },
-        {
-            Name: 'Штраф и неустойка контрагенту',
-            Id: 3
-        },
-        {
-            Name: 'Выплата зарплаты',
-            Id: 4
-        }
-    ];
+	$scope.Model.PaymentTypeCatalog = [
+		{
+			Name: 'Оплата контрагенту',
+			Id: 1
+		},
+		{
+			Name: 'Возврат контрагенту',
+			Id: 2
+		},
+		{
+			Name: 'Штраф и неустойка контрагенту',
+			Id: 3
+		},
+		{
+			Name: 'Выплата зарплаты',
+			Id: 4
+		}
+	];
 
-    $scope.Model.PaymentType = $scope.Model.PaymentTypeCatalog[0];
+	$scope.Model.PaymentType = $scope.Model.PaymentTypeCatalog[0];
 
-    $scope.saveTestForm = function (disabled) {
-        if (disabled) return;
+	$scope.saveTestForm = function (disabled) {
+		if (disabled) return;
 
-        $scope.sending = true;
+		$scope.sending = true;
 
-        $timeout(function (){
-            $scope.sending = false;
-            $timeout(function (){
-                alert('Сохранение успешно');
-            });
-        }, 2000);
-    };
+		$timeout(function () {
+			$scope.sending = false;
+			$timeout(function () {
+				alertBox.push('Сохранение успешно');
+			});
+		}, 2000);
+	};
 
-    $scope.checkPaymentType = function (){
-        return $scope.Model.PaymentType.Id == 4;
-    };
+	$scope.checkPaymentType = function () {
+		return $scope.Model.PaymentType.Id == 4;
+	};
 });
 ;/**
  *  @ngdoc directive
