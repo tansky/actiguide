34b0531b7545374d6997d52c61391990972bc9dc
buhaev <fanual@mail.ru>
2014-04-23 17:24:46 +0400

    Добавлены радио-инпуты, чекбокс, размеры селектов, директива js-placeholder

--- out/app.js
+++ out/app.js
@@ -1427,6 +1427,28 @@ actiGuide.mainModule.directive('hint', function($timeout) {
     };
 });;/**
  *  @ngdoc directive
+ *  @name jsPlaceholder
+ *  @restrict E
+ *
+ *  @description js-placeholder. По клику ставится фокус в элемент с заданным name
+ *
+ *  @param {string} [jsPlaceholder] Name элемента, в который нужно установить фокус
+ *
+ *  TODO: требует доработки
+ */
+actiGuide.mainModule.directive('jsPlaceholder', function() {
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attrs) {
+            var parent = $element.parents('form').length ? $element.parents('form') : 'body';
+            $element.on('click', function() {
+                $('[name="' + $attrs.jsPlaceholder + '"]', parent).focus();
+            });
+        }
+    };
+});
+;/**
+ *  @ngdoc directive
  *  @name moneyField
  *  @restrict EA
  *
